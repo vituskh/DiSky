d52436d9979d93fa4f6dce7a12b1fb9f59b91234
BuildTools <unconfigured@null.spigotmc.org>
2021-03-05 21:54:05 +0100

    :sparkles: Added way to shutdown a specific bot

---
+++ src/main/java/info/itsthesky/Vixio3/skript/effects/EffShutdownBot.java
@@ -0,0 +1,49 @@
+package info.itsthesky.Vixio3.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import info.itsthesky.Vixio3.managers.BotManager;
+import info.itsthesky.Vixio3.tools.Utils;
+import org.bukkit.event.Event;
+
+@Name("Shutdown Discord Bot")
+@Description("Close JDA instance and shutdown the specific bot." +
+        "\n You can't use it as discord bot after shutdown it!")
+@Examples("shutdown the bot named \"MyBot\"")
+@Since("1.0")
+public class EffShutdownBot extends Effect {
+
+    static {
+        Skript.registerEffect(EffShutdownBot.class,
+                "["+ Utils.getPrefixName() +"] (stop|shutdown|close instance of) [the] bot (named|with name) %string%");
+    }
+
+    private Expression<String> exprName;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        exprName = (Expression<String>) exprs[0];
+        return true;
+    }
+
+    @Override
+    protected void execute(Event e) {
+        String name = exprName.getSingle(e);
+        if (name == null) return;
+        BotManager.removeAndShutdown(name);
+    }
+
+    @Override
+    public String toString(Event e, boolean debug) {
+        return "shutdown the bot named " + exprName.toString(e, debug);
+    }
+
+}
