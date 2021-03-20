9d64a0b6e31da0b0852808d43628f6742643365d
BuildTools <unconfigured@null.spigotmc.org>
2021-03-20 15:54:55 +0100

    :sparkles: Added condition to see if a bot is loaded

---
+++ src/main/java/info/itsthesky/DiSky/skript/expressions/bot/ExprIsLoaded.java
@@ -0,0 +1,67 @@
+package info.itsthesky.DiSky.skript.expressions.bot;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import info.itsthesky.DiSky.managers.BotManager;
+import info.itsthesky.DiSky.tools.Utils;
+import net.dv8tion.jda.api.entities.User;
+import org.bukkit.event.Event;
+
+@Name("Is a bot loaded")
+@Description("See if a specific bot is loaded on the server or not.")
+@Examples("if bot \"name\" is loaded:")
+@Since("1.4")
+public class ExprIsLoaded extends SimpleExpression<Boolean> {
+
+	static {
+		Skript.registerExpression(ExprIsLoaded.class, Boolean.class, ExpressionType.SIMPLE,
+				"["+ Utils.getPrefixName() +"] bot [(with name|named)] %string% (is|was) (loaded|online) [on the server]",
+		"["+ Utils.getPrefixName() +"] bot [(with name|named)] %string% (isn't|is not|wasn't|was not) (loaded|online) [on the server]");
+	}
+
+	private Expression<String> exprName;
+	private int pattern;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+		exprName = (Expression<String>) exprs[0];
+		pattern = matchedPattern;
+		return true;
+	}
+
+	@Override
+	protected Boolean[] get(Event e) {
+		String name = exprName.getSingle(e);
+		if (name == null) return new Boolean[] {false};
+		if (pattern == 0) {
+			return new Boolean[] {BotManager.getBots().containsKey(name)};
+		} else {
+			return new Boolean[] {!BotManager.getBots().containsKey(name)};
+		}
+	}
+
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+
+	@Override
+	public Class<? extends Boolean> getReturnType() {
+		return Boolean.class;
+	}
+
+	@Override
+	public String toString(Event e, boolean debug) {
+		return "bot named " + exprName.toString(e, debug) + " is loaded";
+	}
+
+}
