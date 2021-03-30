aba1814b4514e89f7081245a63502aeed796dc6e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-30 20:19:33 +0200

    :bug: Fixed another sync bug for events

--- src/main/java/info/itsthesky/DiSky/skript/events/skript/EventBotJoin.java
+++ src/main/java/info/itsthesky/DiSky/skript/events/skript/EventBotJoin.java
@@ -8,6 +8,7 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.util.SimpleEvent;
 import ch.njol.skript.registrations.EventValues;
 import ch.njol.skript.util.Getter;
+import info.itsthesky.DiSky.tools.Utils;
 import net.dv8tion.jda.api.entities.Guild;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
