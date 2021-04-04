cc16b2fb507d9a62863ea6fed4da4b934d9516f7
BuildTools <unconfigured@null.spigotmc.org>
2021-04-04 20:30:43 +0200

    :sparkles: Added "track author" expression

--- src/main/java/info/itsthesky/DiSky/skript/expressions/embed/ExprEmbedAuthor.java
+++ src/main/java/info/itsthesky/DiSky/skript/expressions/embed/ExprEmbedAuthor.java
@@ -7,30 +7,37 @@ import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.util.coll.CollectionUtils;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import org.bukkit.event.Event;
 
 import javax.annotation.Nullable;
 
-@Name("Embed Author")
-@Description("Set or clear the author of an embed")
+@Name("Author of Track / Embed")
+@Description("Set or clear the author of an embed, and get the track's one.")
 @Examples({"set author of {_embed} to \"ItsTheSky\"",
-        "clear author of {_embed}"})
+        "clear author of {_embed}",
+        "set {_author} to author of last played track"
+})
 @Since("1.4.3")
-public class ExprEmbedAuthor extends SimplePropertyExpression<EmbedBuilder, String> {
+public class ExprEmbedAuthor extends SimplePropertyExpression<Object, String> {
 
     static {
         register(ExprEmbedAuthor.class, String.class,
-                "[embed] author",
-                "embed"
+                "[(embed|track)] author",
+                "embed/track"
         );
     }
 
     @Nullable
     @Override
-    public String convert(EmbedBuilder embed) {
-        return embed.build().getFooter().getIconUrl();
+    public String convert(Object entity) {
+        if (entity instanceof EmbedBuilder) {
+            return ((EmbedBuilder) entity).build().getAuthor().getName();
+        } else {
+            return ((AudioTrack) entity).getInfo().author;
+        }
     }
 
     @Override
@@ -57,7 +64,9 @@ public class ExprEmbedAuthor extends SimplePropertyExpression<EmbedBuilder, Stri
         if (delta == null || delta[0] == null) return;
         switch (mode) {
             case RESET:
-                for (EmbedBuilder embed : getExpr().getArray(e)) {
+                for (Object entity : getExpr().getArray(e)) {
+                    if (entity instanceof AudioTrack) return;
+                    EmbedBuilder embed = (EmbedBuilder) entity;
                     MessageEmbed builded = embed.build();
                     embed.setAuthor(
                             null,
@@ -68,7 +77,9 @@ public class ExprEmbedAuthor extends SimplePropertyExpression<EmbedBuilder, Stri
                 break;
             case SET:
                 String value = delta[0].toString();
-                for (EmbedBuilder embed : getExpr().getArray(e)) {
+                for (Object entity : getExpr().getArray(e)) {
+                    if (entity instanceof AudioTrack) return;
+                    EmbedBuilder embed = (EmbedBuilder) entity;
                     MessageEmbed builded = embed.build();
                     embed.setAuthor(
                             value,
