57d5dec442768c6e632c43fe72d2b5180fefd31c
Sky <livegogo.16334@gmail.com>
2021-04-08 15:42:12 +0200

    :sparkles: Added pin effect and expression

---
+++ src/main/java/info/itsthesky/DiSky/skript/effects/EffPinMessage.java
@@ -0,0 +1,70 @@
+package info.itsthesky.DiSky.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.Variable;
+import ch.njol.util.Kleenean;
+import info.itsthesky.DiSky.DiSky;
+import info.itsthesky.DiSky.managers.BotManager;
+import info.itsthesky.DiSky.skript.expressions.messages.ExprLastMessage;
+import info.itsthesky.DiSky.tools.DiSkyErrorHandler;
+import info.itsthesky.DiSky.tools.Utils;
+import info.itsthesky.DiSky.tools.object.messages.Channel;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import org.bukkit.event.Event;
+
+@Name("Pin / Unpin Message")
+@Description("Pin or unpin any message from a channel.")
+@Examples("pin event-message")
+@Since("1.6")
+public class EffPinMessage extends Effect {
+
+    static {
+        Skript.registerEffect(EffPinMessage.class,
+                "["+ Utils.getPrefixName() +"] pin [discord] [message] %message% [in channel]",
+                "["+ Utils.getPrefixName() +"] unpin [discord] [message] %message% [in channel]"
+        );
+    }
+
+    private Expression<Message> exprMessage;
+    private int pattern;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        exprMessage = (Expression<Message>) exprs[0];
+        pattern = matchedPattern;
+        return true;
+    }
+
+    @Override
+    protected void execute(Event e) {
+        DiSkyErrorHandler.executeHandleCode(e, Event -> {
+            Message message = exprMessage.getSingle(e);
+            if (message == null) return;
+            if (pattern == 0) {
+                message.pin().queue();
+            } else {
+                message.unpin().queue();
+            }
+        });
+    }
+
+    @Override
+    public String toString(Event e, boolean debug) {
+        return "pin or unpin message " + exprMessage.toString(e, debug);
+    }
+
+}
---
+++ src/main/java/info/itsthesky/DiSky/skript/expressions/messages/ExprPinnedMessages.java
@@ -0,0 +1,58 @@
+package info.itsthesky.DiSky.skript.expressions.messages;
+
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.util.coll.CollectionUtils;
+import info.itsthesky.DiSky.tools.MultiplyPropertyExpression;
+import info.itsthesky.DiSky.tools.object.messages.Channel;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import org.bukkit.event.Event;
+
+import javax.annotation.Nullable;
+
+@Name("Pinned Messages")
+@Description("Get all pinned message of a specific text channel.")
+@Examples("set {_msg::*} to pinned message of event-channel")
+@Since("1.0")
+public class ExprPinnedMessages extends MultiplyPropertyExpression<Object, Message> {
+
+    static {
+        register(ExprPinnedMessages.class, Message.class,
+                "pin[ed] (message[s]|msg)",
+                "channel/textchannel"
+        );
+    }
+
+    @Nullable
+    @Override
+    public Message[] convert(Object entity) {
+        if (entity instanceof Channel) return ((Channel) entity).getTextChannel().retrievePinnedMessages().complete().toArray(new Message[0]);
+        if (entity instanceof TextChannel) return ((TextChannel) entity).retrievePinnedMessages().complete().toArray(new Message[0]);
+        return null;
+    }
+
+    @Override
+    public Class<? extends Message> getReturnType() {
+        return Message.class;
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "pinned messages";
+    }
+
+    @Nullable
+    @Override
+    public Class<?>[] acceptChange(Changer.ChangeMode mode) {
+        return CollectionUtils.array();
+    }
+
+    @Override
+    public void change(Event e, @Nullable Object[] delta, Changer.ChangeMode mode) {
+
+    }
+}
