8c5d6a5f06a1d4c8c55e1706e95f78f116efc56f
Sky <livegogo.16334@gmail.com>
2021-04-16 17:35:20 +0200

    :zap: Optimized the getter for reply effect

--- src/main/java/info/itsthesky/DiSky/skript/effects/EffReplyWith.java
+++ src/main/java/info/itsthesky/DiSky/skript/effects/EffReplyWith.java
@@ -25,6 +25,7 @@ import net.dv8tion.jda.api.commands.CommandHook;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
 import org.apache.commons.lang.Validate;
 import org.bukkit.event.Event;
 
@@ -69,110 +70,114 @@ public class EffReplyWith extends Effect {
     @Override
     protected void execute(Event e) {
         DiSkyErrorHandler.executeHandleCode(e, Event -> {
-            Object message = exprMessage.getSingle(e);
-            if (message == null) return;
-            if (!allowedEvents.contains(e.getEventName())) {
-                DiSky.getInstance().getLogger().severe("You can't use 'reply with' effect without a discord guild based event!");
-                return;
-            }
-            TextChannel channel = null;
-            Message storedMessage;
+            try {
+                Object message = exprMessage.getSingle(e);
+                if (message == null) return;
+                if (!allowedEvents.contains(e.getEventName())) {
+                    DiSky.getInstance().getLogger().severe("You can't use 'reply with' effect without a discord guild based event!");
+                    return;
+                }
+                TextChannel channel = null;
+                Message storedMessage;
 
-            EventPrivateMessage eventPrivate = null;
-            if (e.getEventName().equalsIgnoreCase("EventPrivateMessage")) {
-                eventPrivate = (EventPrivateMessage) e;
-            } else if (e instanceof EventMessageReceive) {
-                channel = ((EventMessageReceive) e).getEvent().getTextChannel();
-            } else if (e instanceof EventCommand) {
-                channel = (TextChannel) ((EventCommand) e).getEvent().getChannel();
-            } else if (e instanceof CommandEvent) {
-                channel = (TextChannel) ((CommandEvent) e).getMessageChannel();
-            } else if (e instanceof EventSlashCommand) {
+                EventPrivateMessage eventPrivate = null;
+                if (e.getEventName().equalsIgnoreCase("EventPrivateMessage")) {
+                    eventPrivate = (EventPrivateMessage) e;
+                } else if (e instanceof EventMessageReceive) {
+                    channel = ((EventMessageReceive) e).getEvent().getTextChannel();
+                } else if (e instanceof EventCommand) {
+                    channel = (TextChannel) ((EventCommand) e).getEvent().getChannel();
+                } else if (e instanceof CommandEvent) {
+                    channel = (TextChannel) ((CommandEvent) e).getMessageChannel();
+                } else if (e instanceof EventSlashCommand) {
             /* Slash command have their own reply system
             They're using webhook instead of user, and we
             need to use that, however Discord will wait forever for an answer :)
              */
-                SlashCommandEvent event = ((EventSlashCommand) e).getEvent();
-                CommandHook hook = event.getHook();
-                hook.setEphemeral(true);
-                if (message instanceof Message) {
-                    hook.sendMessage((Message) message).queue();
-                    return;
-                } else if (message instanceof EmbedBuilder) {
-                    // Because of a JDA's bug, we can't send embed currently via ephemeral message :c
+                    SlashCommandEvent event = ((EventSlashCommand) e).getEvent();
+                    CommandHook hook = event.getHook();
+                    hook.setEphemeral(true);
+                    if (message instanceof Message) {
+                        hook.sendMessage((Message) message).queue();
+                        return;
+                    } else if (message instanceof EmbedBuilder) {
+                        // Because of a JDA's bug, we can't send embed currently via ephemeral message :c
                 /* MessageEmbed embed = ((EmbedBuilder) message).build();
                 System.out.println(embed);
                 hook.sendMessage(embed).queue(); */
-                    DiSky.getInstance().getLogger()
-                            .warning("Replying with Embed in slash command are currently not supported! See our discord for more information :)");
-                    hook.sendMessage(":warning: Error, see console for more information!").queue();
-                    return;
-                } else if (message instanceof MessageBuilder) {
-                    hook.sendMessage(((MessageBuilder) message).build()).queue(null, DiSkyErrorHandler::logException);
-                    return;
-                } else {
-                    hook.sendMessage(message.toString()).queue(null, DiSkyErrorHandler::logException);
-                    return;
+                        DiSky.getInstance().getLogger()
+                                .warning("Replying with Embed in slash command are currently not supported! See our discord for more information :)");
+                        hook.sendMessage(":warning: Error, see console for more information!").queue();
+                        return;
+                    } else if (message instanceof MessageBuilder) {
+                        hook.sendMessage(((MessageBuilder) message).build()).queue(null, DiSkyErrorHandler::logException);
+                        return;
+                    } else {
+                        hook.sendMessage(message.toString()).queue(null, DiSkyErrorHandler::logException);
+                        return;
+                    }
                 }
-            }
 
-            boolean isFromPrivate = false;
-            if (eventPrivate != null) isFromPrivate = true;
+                boolean isFromPrivate = false;
+                if (eventPrivate != null) isFromPrivate = true;
 
-            if (message instanceof Message) {
-                if (isFromPrivate) {
-                    storedMessage = eventPrivate
-                            .getEvent()
-                            .getPrivateChannel()
-                            .sendMessage((Message) message).complete();
+                if (message instanceof Message) {
+                    if (isFromPrivate) {
+                        storedMessage = eventPrivate
+                                .getEvent()
+                                .getPrivateChannel()
+                                .sendMessage((Message) message).complete(true);
+                    } else {
+                        storedMessage = channel.getJDA()
+                                .getTextChannelById(
+                                        channel.getId()
+                                ).sendMessage((Message) message).complete(true);
+                    }
+                } else if (message instanceof EmbedBuilder) {
+                    if (isFromPrivate) {
+                        storedMessage = eventPrivate
+                                .getEvent()
+                                .getPrivateChannel()
+                                .sendMessage(((EmbedBuilder) message).build()).complete(true);
+                    } else {
+                        storedMessage = channel.getJDA()
+                                .getTextChannelById(
+                                        channel.getId()
+                                ).sendMessage(((EmbedBuilder) message).build()).complete(true);
+                    }
+                }  else if (message instanceof MessageBuilder) {
+                    if (isFromPrivate) {
+                        storedMessage = eventPrivate
+                                .getEvent()
+                                .getPrivateChannel()
+                                .sendMessage(((MessageBuilder) message).build()).complete(true);
+                    } else {
+                        storedMessage = channel.getJDA()
+                                .getTextChannelById(
+                                        channel.getId()
+                                ).sendMessage(((MessageBuilder) message).build()).complete(true);
+                    }
                 } else {
-                    storedMessage = channel.getJDA()
-                            .getTextChannelById(
-                                    channel.getId()
-                            ).sendMessage((Message) message).complete();
+                    if (isFromPrivate) {
+                        storedMessage = eventPrivate
+                                .getEvent()
+                                .getPrivateChannel()
+                                .sendMessage(message.toString()).complete(true);
+                    } else {
+                        storedMessage = channel.getJDA()
+                                .getTextChannelById(
+                                        channel.getId()
+                                ).sendMessage(message.toString()).complete(true);
+                    }
                 }
-            } else if (message instanceof EmbedBuilder) {
-                if (isFromPrivate) {
-                    storedMessage = eventPrivate
-                            .getEvent()
-                            .getPrivateChannel()
-                            .sendMessage(((EmbedBuilder) message).build()).complete();
-                } else {
-                    storedMessage = channel.getJDA()
-                            .getTextChannelById(
-                                    channel.getId()
-                            ).sendMessage(((EmbedBuilder) message).build()).complete();
-                }
-            }  else if (message instanceof MessageBuilder) {
-                if (isFromPrivate) {
-                    storedMessage = eventPrivate
-                            .getEvent()
-                            .getPrivateChannel()
-                            .sendMessage(((MessageBuilder) message).build()).complete();
-                } else {
-                    storedMessage = channel.getJDA()
-                            .getTextChannelById(
-                                    channel.getId()
-                            ).sendMessage(((MessageBuilder) message).build()).complete();
-                }
-            } else {
-                if (isFromPrivate) {
-                    storedMessage = eventPrivate
-                            .getEvent()
-                            .getPrivateChannel()
-                            .sendMessage(message.toString()).complete();
-                } else {
-                    storedMessage = channel.getJDA()
-                            .getTextChannelById(
-                                    channel.getId()
-                            ).sendMessage(message.toString()).complete();
-                }
+                ExprLastMessage.lastMessage = storedMessage;
+                if (exprVar == null) return;
+                if (!exprVar.getClass().getName().equalsIgnoreCase("ch.njol.skript.lang.Variable")) return;
+                Variable var = (Variable) exprVar;
+                Utils.setSkriptVariable(var, storedMessage, e);
+            } catch (RateLimitedException ex) {
+                DiSky.getInstance().getLogger().severe("DiSky tried to get a message, but was rate limited.");
             }
-            ExprLastMessage.lastMessage = storedMessage;
-            if (exprVar == null) return;
-            if (!exprVar.getClass().getName().equalsIgnoreCase("ch.njol.skript.lang.Variable")) return;
-            Variable var = (Variable) exprVar;
-            Utils.setSkriptVariable(var, storedMessage, e);
         });
     }
 
