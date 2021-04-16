5f23c43e0e4fd32c0b039cfa149ce88992ba8268
Sky <livegogo.16334@gmail.com>
2021-04-16 17:35:56 +0200

    :bug: Fixed and optimized send message effect

--- src/main/java/info/itsthesky/DiSky/skript/effects/EffSendMessage.java
+++ src/main/java/info/itsthesky/DiSky/skript/effects/EffSendMessage.java
@@ -21,6 +21,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
 import org.bukkit.event.Event;
 
 import java.util.Objects;
@@ -32,7 +33,7 @@ import java.util.Objects;
         "\t\tset title of embed to \"The bot has been started!\"\n" +
         "\t\tset color of embed to green\n" +
         "\t\tset timestamp of embed to now\n" +
-        "\tsend last embed to channel with id \"818182473502294066\"")
+        "\tsend last embed to text channel with id \"818182473502294066\"")
 @Since("1.0")
 public class EffSendMessage extends Effect {
 
@@ -61,68 +62,62 @@ public class EffSendMessage extends Effect {
     @Override
     protected void execute(Event e) {
         DiSkyErrorHandler.executeHandleCode(e, Event -> {
-            Object channel = exprChannel.getSingle(e);
-            Object content = exprMessage.getSingle(e);
-            if (channel == null || content == null) return;
-            Message storedMessage;
+            try {
+                Object channel = exprChannel.getSingle(e);
+                Object content = exprMessage.getSingle(e);
+                if (channel == null || content == null) return;
+                Message storedMessage;
 
-            TextChannel channel1 = null;
-            if (channel1 instanceof TextChannel) channel1 = (TextChannel) channel;
-            if (channel instanceof GuildChannel && ((GuildChannel) channel).getType().equals(ChannelType.TEXT)) channel1 = (TextChannel) channel;
+                TextChannel channel1 = null;
+                if (channel instanceof TextChannel) channel1 = (TextChannel) channel;
+                if (channel instanceof GuildChannel && ((GuildChannel) channel).getType().equals(ChannelType.TEXT)) channel1 = (TextChannel) channel;
 
-            if (channel instanceof User || channel instanceof Member) {
-                User user;
-                if (channel instanceof Member) {
-                    user = ((Member) channel).getUser();
-                } else {
-                    user = (User) channel;
+                if (channel instanceof User || channel instanceof Member) {
+                    User user;
+                    if (channel instanceof Member) {
+                        user = ((Member) channel).getUser();
+                    } else {
+                        user = (User) channel;
+                    }
+                    if (content instanceof EmbedBuilder) {
+                        storedMessage = user.openPrivateChannel()
+                                .flatMap(channel2 -> channel2.sendMessage(((EmbedBuilder) content).build()))
+                                .complete(true);
+                    } else if (content instanceof MessageBuilder) {
+                        storedMessage = user.openPrivateChannel()
+                                .flatMap(channel2 -> channel2.sendMessage(((MessageBuilder) content).build()))
+                                .complete(true);
+                    } else {
+                        storedMessage = user.openPrivateChannel()
+                                .flatMap(channel2 -> channel2.sendMessage(content.toString()))
+                                .complete(true);
+                    }
+                    ExprLastMessage.lastMessage = storedMessage;
+                    if (exprVar == null) return;
+                    if (!exprVar.getClass().getName().equalsIgnoreCase("ch.njol.skript.lang.Variable")) return;
+                    Variable var = (Variable) exprVar;
+                    Utils.setSkriptVariable(var, storedMessage, e);
                 }
+
+                if (channel1 == null) return;
+                if (!Utils.areJDASimilar(channel1.getJDA(), exprName == null ? null : exprName.getSingle(e))) return;
+
+                System.out.println("debug 6");
                 if (content instanceof EmbedBuilder) {
-                    storedMessage = user.openPrivateChannel()
-                            .flatMap(channel2 -> channel2.sendMessage(((EmbedBuilder) content).build()))
-                            .complete();
+                    storedMessage = channel1.sendMessage(((EmbedBuilder) content).build()).complete();
                 } else if (content instanceof MessageBuilder) {
-                    storedMessage = user.openPrivateChannel()
-                            .flatMap(channel2 -> channel2.sendMessage(((MessageBuilder) content).build()))
-                            .complete();
+                    storedMessage = channel1.sendMessage(((MessageBuilder) content).build()).complete();
                 } else {
-                    storedMessage = user.openPrivateChannel()
-                            .flatMap(channel2 -> channel2.sendMessage(content.toString()))
-                            .complete();
+                    storedMessage = channel1.sendMessage(content.toString()).complete();
                 }
                 ExprLastMessage.lastMessage = storedMessage;
                 if (exprVar == null) return;
                 if (!exprVar.getClass().getName().equalsIgnoreCase("ch.njol.skript.lang.Variable")) return;
                 Variable var = (Variable) exprVar;
                 Utils.setSkriptVariable(var, storedMessage, e);
-            } else return;
-
-            if (channel1 == null) return;
-            JDA bot = null;
-            if (exprName != null) {
-                bot = BotManager.getBot(exprName.getSingle(e));
-            } else {
-                bot = channel1.getJDA();
+            } catch (RateLimitedException ex) {
+                DiSky.getInstance().getLogger().severe("DiSky tried to get a message, but was rate limited.");
             }
-            if (bot == null) return;
-
-            TextChannel channel2 = bot.getTextChannelById(channel1.getId());
-            if (channel2 == null) {
-                DiSky.getInstance().getLogger().severe("Cannot get the right text channel with id '"+channel1.getId()+"'!");
-                return;
-            }
-            if (content instanceof EmbedBuilder) {
-                storedMessage = channel2.sendMessage(((EmbedBuilder) content).build()).complete();
-            } else if (content instanceof MessageBuilder) {
-                storedMessage = channel2.sendMessage(((MessageBuilder) content).build()).complete();
-            } else {
-                storedMessage = channel2.sendMessage(content.toString()).complete();
-            }
-            ExprLastMessage.lastMessage = storedMessage;
-            if (exprVar == null) return;
-            if (!exprVar.getClass().getName().equalsIgnoreCase("ch.njol.skript.lang.Variable")) return;
-            Variable var = (Variable) exprVar;
-            Utils.setSkriptVariable(var, storedMessage, e);
         });
     }
 
